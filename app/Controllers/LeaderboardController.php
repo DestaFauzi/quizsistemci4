514898a74a93f96db5ce68131256f270c14688c5
khmalz <akmalrma1710@gmail.com>
2025-06-10 13:17:41 +0700

    delete unused controller files

--- app/Controllers/LeaderboardController.php
+++ app/Controllers/LeaderboardController.php
@@ -9,11 +9,11 @@ class LeaderboardController extends Controller
 {
     public function showLeaderboard($quizId)
     {
+        // Ambil data leaderboard berdasarkan quiz_id
         $quizResultsModel = new QuizResultsModel();
-        $results = $quizResultsModel->where('quiz_id', $quizId)
-            ->orderBy('score', 'DESC')
-            ->findAll();
+        $results = $quizResultsModel->getLeaderboard($quizId);  // Mendapatkan data leaderboard
 
+        // Kirim data leaderboard ke view
         return view('murid/leaderboard', ['leaderboard' => $results]);
     }
 }
